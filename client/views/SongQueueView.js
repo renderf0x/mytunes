900212d67a6521bc528ccd5193659b22e735e71f
renderf0x <derek@barncard.com>
2014-11-12 17:01:06 -0800

    Added song model events, created SongQueue Views, enabled pushing songs to queue, added listeners for enqueue events

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,29 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.render();
+    this.listenTo(this.collection, "add", this.render);
   },
 
   render: function() {
+    console.log('render fired at SongQueueView');
+    this.$el.children().detach();
+
+    this.$el.html('<th>SongQueue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+
     return this.$el;
   }
 
 });
+
+// params.library.on('enqueue', function(song){
+//   this.get('songQueue').push(song);
+//   console.log(this.get('songQueue').length);
+// }, this);
